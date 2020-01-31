ac6e29e7a1b26f159cba332a2b7ea50394f07771
gsindlinger <60215105+gsindlinger@users.noreply.github.com>
2020-01-31 18:57:13 +0100

    Update hilfsprozeduren.js

--- hilfsprozeduren.js
+++ hilfsprozeduren.js
@@ -100,7 +100,7 @@ function changeArrowRotationOnOrientationChange() {
   
   arrow = document.getElementById("popupDeviationSpecialHeader")
   console.log(arrow.style.transform)
-  if(smartphone[0].matches) {
+  if(smartphone[1].matches) {
     console.log("Test")
     if(arrow.style.transform == "rotate(180deg)") {
       arrow.style.transform = "rotate(270deg)"
