c0ffc2dd004f5008a7ef46109483592098a0f73e
gsindlinger <60215105+gsindlinger@users.noreply.github.com>
2020-03-13 21:33:05 +0100

    Changed Folder Structure

---
+++ js/main.js
@@ -0,0 +1,122 @@
+window.addEventListener("orientationchange", function() {
+  //changeArrowRotationOnOrientationChange()
+  updateValue()
+})
+window.addEventListener("resize", resizeChart)
+window.addEventListener("DOMContentLoaded", function() {
+  fillRente(getEinmalbetrag(), getPerformance(), calcLaufzeit(), true)
+  updateValue()
+  if(smartphone[0].matches || smartphone[1].matches || smartphone[2].matches) {
+    openSliderPopup(0)
+  }
+  funcSmartphone()
+  
+})
+
+for (binding of bindings) {
+  const input = binding.input
+  const output = binding.output
+
+  if(smartphone[3].matches) {
+    input.addEventListener('change', updateValue)
+  }
+  input.addEventListener('input', updateDisplay.bind(binding))
+  updateDisplay.apply(binding, [true])
+}
+
+function updateValue() {
+  calculateViz(getPerformance(),getDeviation(), getEinmalbetrag(),
+  getStartAge(),getEndAge())
+}
+
+function updateDisplay(shouldPreventUpdateValue) {
+  const input = this.input
+  const output = this.output
+  let renteAlterStart = parseInt(renteneintrittsalter.input.value)
+  let renteAlterEnde = parseInt(rentenaustrittsalter.input.value)
+
+  let step
+  
+  if(isNaN(parseInt(input.step))) {
+    step = 1
+  }else{
+    step = parseInt(input.step)
+  }
+
+
+  if(input.id == "rangeRenditeerwartung") {
+    output.innerText = input.value + "%"
+
+    if(checkMuSigma.checked == false) {
+      standardabweichung.input.disabled = true;
+
+      let sigma = sigmaFromPerformance(parseInt(input.value))
+      //from https://stackoverflow.com/questions/8584902/get-closest-number-out-of-array
+      let closest = closestNumberInArrayIndex(sigma, sigmaArray)
+      standardabweichung.input.value = closest
+      standardabweichung.output.innerText = textDeviation(parseInt(closest))
+    }
+  }else if(input.id == "rangeEinmalbetrag") {
+    if(parseInt(input.value) <= parseInt(renteRange.input.value)*12) {
+      window.alert("Die Entnahme darf maximal den Wert der Rente besitzen!")
+      console.log("step ", step)
+      console.log("input Value ", input.value)
+      input.value = parseInt(renteRange.input.value) + step
+    }
+    output.innerText = numberWithPoints(input.value) + "€"
+  }else if(input.id == "rangeStandardabweichung" && checkMuSigma.checked == true) {
+    output.innerText = textDeviation(parseInt(input.value))
+
+
+
+
+    /*Verhindern, dass Renteintrittsalter und -austrittsalter sich fälschlicher-
+    weise überschneiden*/
+  }else if(input.id == "rangeRenteneintrittsalter") {
+      if(parseInt(input.value) >= renteAlterEnde) {
+        window.alert("Das Rentenaustrittsalter muss mindestens ein Jahr über dem Renteneintrittsalter liegen!")
+        input.value = renteAlterEnde - step
+        renteAlterStart = renteAlterEnde - step
+      }
+      output.innerText = input.value
+      renteAlterStart = parseInt(input.value)
+
+      rentenaustrittsalter.input.min = parseInt(input.value) + step
+
+    }else if(input.id == "rangeRentenaustrittsalter") {
+      if(parseInt(input.value) <= renteAlterStart) {
+          window.alert("Das Rentenaustrittsalter muss mindestens ein Jahr über dem Renteneintrittsalter liegen!")
+          input.value = renteAlterStart + step
+          output.innerText = input.value
+      }
+      output.innerText = input.value
+      renteAlterEnde = parseInt(input.value)
+
+      renteneintrittsalter.input.max = Math.min(parseInt(input.value) - 1,75)
+      fillPercentageHeader(renteAlterEnde)
+
+
+    }else if(input.id == "rangeRente") {
+      if(parseInt(input.value)*12 >= parseInt(einmalbetrag.input.value)) {
+        window.alert("Die Entnahme darf maximal den Wert der Rente besitzen!")
+        input.value = einmalbetrag.input.value - step
+      }
+      output.innerText = numberWithPoints(input.value) + "€"
+      console.log("Test")
+      //checkBordersForInputRange()
+    }else{
+      output.innerText = input.value
+    }
+
+    /*Ersetzen des Textes in den Boxen*/
+
+  if (input.id != "rangeRente") {
+    fillRente(getEinmalbetrag(),getPerformance(),renteAlterEnde-renteAlterStart, false)
+  }
+
+
+  if(!smartphone[3].matches && shouldPreventUpdateValue !== true) {
+    updateValue()
+  }
+
+}
---
+++ js/mediaQueries.js
@@ -0,0 +1,131 @@
+/*Festlegen relevanter Mediaqueries*/
+const smartphone = [window.matchMedia("(orientation: portrait)"),
+                    window.matchMedia("(orientation: landscape) and (max-width: 1024px)"),
+                    window.matchMedia("(pointer: coarse) and (orientation: landscape)"),
+                    window.matchMedia("(pointer: coarse)"),
+                    window.matchMedia("(min-aspect-ratio: 2/1) and (max-width: 899px)"),
+                    window.matchMedia("(min-aspect-ratio: 15/9)"),
+                    window.matchMedia("(min-aspect-ratio: 2/1) and (max-width: 1600px)"),
+                    window.matchMedia("(max-aspect-ratio: 4/5) and (max-width: 500px)"),
+                    window.matchMedia("(min-aspect-ratio: 3/2) and (max-width: 400px)")]
+
+
+for (let i = 0; i < smartphone.length; i++) {
+    funcSmartphone()
+    smartphone[i].addListener(funcSmartphone)
+}
+
+
+
+/*Grundlegende Funktion, welche die Anpassungen zwischen mobilem und nicht-mobilen Endgerät
+vornimmt. Das heißt insbesondere ein schmaleres Logo und das Umhängen der Input-Felder an die Kopfzeile
+im Falle eines mobilen Endgeräts. Analog dazu muss dies natürlich auch umgekehrt beim Wechsel
+von mobilem Gerät zu nicht mobilem Gerät rückgängig gemacht werden.*/
+
+function funcSmartphone() {
+
+    let popupDeviation = document.getElementById("mainPopupDeviation").querySelectorAll("p")
+    let logo = document.getElementById("imgLogo")
+    let titleHeader = document.getElementById("title").querySelectorAll("h1")
+
+    if(smartphone[0].matches) {
+        //Zeilenumbruch in Titel hinzufügen falls das Endgerät länger als breit ist
+        titleHeader[0].innerHTML = "Berechne Sie <br> Ihren Entnahmeplan"
+    }else{
+        titleHeader[0].innerHTML = "Berechne Sie Ihren Entnahmeplan"
+    }
+    
+
+
+    if(smartphone[0].matches || smartphone[1].matches || smartphone[2].matches) {
+
+         //Anpassen des Logos
+         logo.src = "Uni_Ulm_Logo_Ausschnitt.svg"
+    
+        //Anhängen der Input-Felder an den HeaderSpecial
+        for (let i = 0; i < bindings.length; i++) {
+
+            let parent = bindings[i].input.parentElement.parentElement
+            
+            let headerBox = headerSpecial[i].querySelector(".popupHeaderSpecial")
+            headerBox.appendChild(parent)
+            if(i == 4) {
+                let extraChild = headerBox.children[2]
+                headerBox.children[2].remove
+                headerBox.appendChild(extraChild)
+                headerBox.appendChild(mainPopupDeviation)
+            }
+
+        }
+
+        //Anpassen des Textes im Popup-Deviation
+        popupDeviation[0].innerHTML = ""
+
+        
+
+
+
+    }else{
+
+
+        //Anpassen des Logos
+        logo.src = "Uni_ulm_logo.svg"
+
+        //Anhängen der Input-Felder an den ursprünglichen Platz in der HTML-Struktur
+        if(headerSpecial[4].querySelector(".popupHeaderSpecial").childElementCount >= 4) {
+            for (let i = 0; i < headerNormal.length; i++) {
+                let box = headerSpecial[i].querySelector(".popupHeaderSpecial")
+
+                if(i == 4) {
+                    
+                    document.getElementById("popupDeviation").append(mainPopupDeviation)
+                    console.log(box.children)
+                    let extraChild = box.children[2]
+                    headerNormal[i].children[1].prepend(extraChild)
+                    let extraChild2 = box.children[2]
+                    box.append(extraChild2)
+
+
+                }else{
+                    box = box.lastElementChild
+                    headerNormal[i].appendChild(box)
+
+                }
+               
+                 
+            }
+        }
+
+         //Anpassen des Textes im Popup-Deviation
+         popupDeviation[0].innerHTML = "Das <b>Risiko der Anlage</b> hängt von der gewählten Rendite ab.<br>"
+        
+        
+
+
+    }
+
+}
+
+
+/*Funktion, welche das Öffnen und Schließen der Input-Felder im Falle eines
+mobilen Endgerät ermöglicht. Alle nicht aktivierten Input-Felder werden geschlossen, das aktivierte dementsprechend
+angezeigt*/
+function openSliderPopup(n) {
+    for(let i = 0; i < headerSpecial.length; i++) {
+        if(i == n) {
+            headerSpecial[n].style.display = "block"
+            headerSpecial[n].parentNode.classList.add("activeInput")
+        }else{
+            closeSliderPopup(i)
+        }
+    }
+}
+
+/*Schließt das gewählte Popup des zugehörigen Input-Feldes im Falle eines mobilen Endgeräts*/
+function closeSliderPopup(n) {
+    headerSpecial[n].style.display = "none"
+    headerSpecial[n].parentNode.classList.remove("activeInput")
+}
+
+
+
